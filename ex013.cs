fb9982f258a26cd27680b876bb59763f98413d9e
Dan <veras_dan@hotmail.com>
2021-09-29 23:33:32 -0300

    update

---
+++ ex013.cs
@@ -0,0 +1,9 @@
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
---
+++ ex014.cs
@@ -0,0 +1,4 @@
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
